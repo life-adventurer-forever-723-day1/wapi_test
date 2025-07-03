a5b14ad90cc4d8e89f29a7a306c7a57cc9b862f1
life-adventurer-forever-723-day1 <abhinay.v2004@gmail.com>
2025-07-03 14:45:55 +0530

    database.py

--- database.py
+++ database.py
@@ -1,10 +1,11 @@
+
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
+# Update this with your actual Railway MySQL URL
 DATABASE_URL = "mysql+pymysql://root:HsKrEZzYTGNIymDGLgHlHxlSzOefsZcm@tramway.proxy.rlwy.net:45052/railway"
 
-
-engine = create_engine(DATABASE_URL)
+engine = create_engine(DATABASE_URL, echo=True)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
