4b780a29c86013043bf7d1992f0500c4994c2618
life-adventurer-forever-723-day1 <abhinay.v2004@gmail.com>
2025-07-03 15:10:12 +0530

    dont_open.txt

--- telegram_notif.py
+++ telegram_notif.py
@@ -1,15 +1,1 @@
-import requests
-import os
 
-TELEGRAM_BOT_TOKEN = os.getenv("TELEGRAM_BOT_TOKEN", "8059019021:AAEkw9yMFWBfS0xdADV3F9p9xEICHneviTs")
-
-def send_telegram_message(chat_id, message):
-    url = f"https://api.telegram.org/bot{TELEGRAM_BOT_TOKEN}/sendMessage"
-    payload = {
-        "chat_id": chat_id,
-        "text": message
-    }
-    response = requests.post(url, json=payload)
-    if response.status_code != 200:
-        print("❌ Failed to send message:", response.text)
-    return response.json()
