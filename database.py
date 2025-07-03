6759c0a420134ed6107c611f25394fd670b34359
life-adventurer-forever-723-day1 <abhinay.v2004@gmail.com>
2025-07-03 13:10:08 +0530

    database.py

--- database.py
+++ database.py
@@ -3,7 +3,8 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
 # ✅ Update this if needed
-DATABASE_URL = "mysql://root:HsKrEZzYTGNIymDGLgHlHxlSzOefsZcm@tramway.proxy.rlwy.net:45052/railway"
+DATABASE_URL = "mysql+pymysql://root:HsKrEZzYTGNIymDGLgHlHxlSzOefsZcm@tramway.proxy.rlwy.net:45052/railway"
+
 
 engine = create_engine(DATABASE_URL, echo=True)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
